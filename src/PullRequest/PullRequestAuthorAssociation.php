b8095fe422324989399b4ba93ab49af94981fd38
Miro Svrtan <miro@mirosvrtan.me>
2018-03-25 09:18:20 +0200

    Add PullRequestAuthorAssociation

---
+++ src/PullRequest/PullRequestAuthorAssociation.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevboardLib\GitHub\PullRequest;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @see \spec\DevboardLib\GitHub\PullRequest\PullRequestAuthorAssociationSpec
+ * @see \Tests\DevboardLib\GitHub\PullRequest\PullRequestAuthorAssociationTest
+ */
+class PullRequestAuthorAssociation extends Enum
+{
+    const COLLABORATOR = 'COLLABORATOR';
+
+    const OWNER = 'OWNER';
+
+    const CONTRIBUTOR = 'CONTRIBUTOR';
+
+    const NONE = 'NONE';
+
+    public function serialize(): string
+    {
+        return $this->value;
+    }
+
+    public static function deserialize(string $value): self
+    {
+        return new self($value);
+    }
+}
---
+++ tests/PullRequest/PullRequestAuthorAssociationTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DevboardLib\GitHub\PullRequest;
+
+use DevboardLib\GitHub\PullRequest\PullRequestAuthorAssociation;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DevboardLib\GitHub\PullRequest\PullRequestAuthorAssociation
+ * @group  todo
+ */
+class PullRequestAuthorAssociationTest extends TestCase
+{
+    /** @var string */
+    private $value;
+
+    /** @var PullRequestAuthorAssociation */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->value = 'COLLABORATOR';
+        $this->sut   = new PullRequestAuthorAssociation($this->value);
+    }
+
+    public function testGetValue()
+    {
+        self::assertSame($this->value, $this->sut->getValue());
+    }
+
+    public function testToString()
+    {
+        self::assertSame($this->value, $this->sut->__toString());
+    }
+
+    public function testSerialize()
+    {
+        self::assertEquals($this->value, $this->sut->serialize());
+    }
+
+    public function testDeserialize()
+    {
+        self::assertEquals($this->sut, $this->sut->deserialize($this->value));
+    }
+}
