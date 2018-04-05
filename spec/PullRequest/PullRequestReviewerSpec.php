a29fc7fb18f1373ba0dc7d9f76d0e637da525da8
Miro Svrtan <miro@mirosvrtan.me>
2018-04-05 17:24:14 +0200

    Make GravatarId nullable in PullRequestReviewer

--- spec/PullRequest/PullRequestReviewerSpec.php
+++ spec/PullRequest/PullRequestReviewerSpec.php
@@ -19,14 +19,14 @@ class PullRequestReviewerSpec extends ObjectBehavior
     public function let(
         AccountId $userId,
         AccountLogin $login,
-        AccountType $gitHubAccountType,
+        AccountType $type,
         AccountAvatarUrl $avatarUrl,
         GravatarId $gravatarId,
         AccountHtmlUrl $htmlUrl,
         AccountApiUrl $apiUrl
     ) {
         $this->beConstructedWith(
-            $userId, $login, $gitHubAccountType, $avatarUrl, $gravatarId, $htmlUrl, $apiUrl, false
+            $userId, $login, $type, $avatarUrl, $gravatarId, $htmlUrl, $apiUrl, $siteAdmin = false
         );
     }
 
@@ -35,22 +35,94 @@ class PullRequestReviewerSpec extends ObjectBehavior
         $this->shouldHaveType(PullRequestReviewer::class);
     }
 
-    public function it_should_expose_all_values_via_getters(
+    public function it_exposes_user_id(AccountId $userId)
+    {
+        $this->getUserId()->shouldReturn($userId);
+    }
+
+    public function it_exposes_login(AccountLogin $login)
+    {
+        $this->getLogin()->shouldReturn($login);
+    }
+
+    public function it_exposes_type(AccountType $type)
+    {
+        $this->getType()->shouldReturn($type);
+    }
+
+    public function it_exposes_avatar_url(AccountAvatarUrl $avatarUrl)
+    {
+        $this->getAvatarUrl()->shouldReturn($avatarUrl);
+    }
+
+    public function it_exposes_gravatar_id(GravatarId $gravatarId)
+    {
+        $this->getGravatarId()->shouldReturn($gravatarId);
+    }
+
+    public function it_exposes_html_url(AccountHtmlUrl $htmlUrl)
+    {
+        $this->getHtmlUrl()->shouldReturn($htmlUrl);
+    }
+
+    public function it_exposes_api_url(AccountApiUrl $apiUrl)
+    {
+        $this->getApiUrl()->shouldReturn($apiUrl);
+    }
+
+    public function it_exposes_is_site_admin()
+    {
+        $this->isSiteAdmin()->shouldReturn(false);
+    }
+
+    public function it_has_gravatar_id()
+    {
+        $this->hasGravatarId()->shouldReturn(true);
+    }
+
+    public function it_can_be_serialized(
         AccountId $userId,
         AccountLogin $login,
-        AccountType $gitHubAccountType,
+        AccountType $type,
         AccountAvatarUrl $avatarUrl,
         GravatarId $gravatarId,
         AccountHtmlUrl $htmlUrl,
         AccountApiUrl $apiUrl
     ) {
-        $this->getUserId()->shouldReturn($userId);
-        $this->getLogin()->shouldReturn($login);
-        $this->getAccountType()->shouldReturn($gitHubAccountType);
-        $this->getAvatarUrl()->shouldReturn($avatarUrl);
-        $this->getGravatarId()->shouldReturn($gravatarId);
-        $this->getHtmlUrl()->shouldReturn($htmlUrl);
-        $this->getApiUrl()->shouldReturn($apiUrl);
-        $this->isSiteAdmin()->shouldReturn(false);
+        $userId->serialize()->shouldBeCalled()->willReturn(583231);
+        $login->serialize()->shouldBeCalled()->willReturn('octocat');
+        $type->serialize()->shouldBeCalled()->willReturn('User');
+        $avatarUrl->serialize()->shouldBeCalled()->willReturn('https://avatars3.githubusercontent.com/u/583231?v=4');
+        $gravatarId->serialize()->shouldBeCalled()->willReturn('45g23r2a');
+        $htmlUrl->serialize()->shouldBeCalled()->willReturn('https://github.com/octocat');
+        $apiUrl->serialize()->shouldBeCalled()->willReturn('https://api.github.com/users/octocat');
+        $this->serialize()->shouldReturn(
+            [
+                'userId'     => 583231,
+                'login'      => 'octocat',
+                'type'       => 'User',
+                'avatarUrl'  => 'https://avatars3.githubusercontent.com/u/583231?v=4',
+                'gravatarId' => '45g23r2a',
+                'htmlUrl'    => 'https://github.com/octocat',
+                'apiUrl'     => 'https://api.github.com/users/octocat',
+                'siteAdmin'  => false,
+            ]
+        );
+    }
+
+    public function it_can_be_deserialized()
+    {
+        $input = [
+            'userId'     => 583231,
+            'login'      => 'octocat',
+            'type'       => 'User',
+            'avatarUrl'  => 'https://avatars3.githubusercontent.com/u/583231?v=4',
+            'gravatarId' => '45g23r2a',
+            'htmlUrl'    => 'https://github.com/octocat',
+            'apiUrl'     => 'https://api.github.com/users/octocat',
+            'siteAdmin'  => false,
+        ];
+
+        $this->deserialize($input)->shouldReturnAnInstanceOf(PullRequestReviewer::class);
     }
 }
