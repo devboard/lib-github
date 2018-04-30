081e641a0a1652b060b3ebc376a23fed5d80a560
Miro Svrtan <miro@mirosvrtan.me>
2018-04-30 21:30:07 +0200

    Task #53: Remove Issue related urls

--- tests/GitHubIssueCommentTest.php
+++ tests/GitHubIssueCommentTest.php
@@ -9,13 +9,10 @@ use DevboardLib\GitHub\Account\AccountId;
 use DevboardLib\GitHub\Account\AccountLogin;
 use DevboardLib\GitHub\Account\AccountType;
 use DevboardLib\GitHub\GitHubIssueComment;
-use DevboardLib\GitHub\Issue\IssueApiUrl;
 use DevboardLib\GitHub\Issue\IssueId;
-use DevboardLib\GitHub\IssueComment\IssueCommentApiUrl;
 use DevboardLib\GitHub\IssueComment\IssueCommentAuthor;
 use DevboardLib\GitHub\IssueComment\IssueCommentBody;
 use DevboardLib\GitHub\IssueComment\IssueCommentCreatedAt;
-use DevboardLib\GitHub\IssueComment\IssueCommentHtmlUrl;
 use DevboardLib\GitHub\IssueComment\IssueCommentId;
 use DevboardLib\GitHub\IssueComment\IssueCommentUpdatedAt;
 use PHPUnit\Framework\TestCase;
@@ -40,15 +37,6 @@ class GitHubIssueCommentTest extends TestCase
     /** @var IssueCommentAuthor */
     private $author;
 
-    /** @var IssueCommentHtmlUrl */
-    private $htmlUrl;
-
-    /** @var IssueCommentApiUrl */
-    private $apiUrl;
-
-    /** @var IssueApiUrl */
-    private $issueApiUrl;
-
     /** @var IssueCommentCreatedAt */
     private $createdAt;
 
@@ -70,21 +58,10 @@ class GitHubIssueCommentTest extends TestCase
             new AccountAvatarUrl('https://avatars.githubusercontent.com/u/6752317?v=3'),
             false
         );
-        $this->htmlUrl     = new IssueCommentHtmlUrl('htmlUrl');
-        $this->apiUrl      = new IssueCommentApiUrl('apiUrl');
-        $this->issueApiUrl = new IssueApiUrl('apiUrl');
-        $this->createdAt   = new IssueCommentCreatedAt('2016-08-02T17:35:14+00:00');
-        $this->updatedAt   = new IssueCommentUpdatedAt('2016-08-02T17:35:14+00:00');
-        $this->sut         = new GitHubIssueComment(
-            $this->id,
-            $this->issueId,
-            $this->body,
-            $this->author,
-            $this->htmlUrl,
-            $this->apiUrl,
-            $this->issueApiUrl,
-            $this->createdAt,
-            $this->updatedAt
+        $this->createdAt = new IssueCommentCreatedAt('2016-08-02T17:35:14+00:00');
+        $this->updatedAt = new IssueCommentUpdatedAt('2016-08-02T17:35:14+00:00');
+        $this->sut       = new GitHubIssueComment(
+            $this->id, $this->issueId, $this->body, $this->author, $this->createdAt, $this->updatedAt
         );
     }
 
@@ -106,11 +83,6 @@ class GitHubIssueCommentTest extends TestCase
     public function testGetAuthor()
     {
         self::assertSame($this->author, $this->sut->getAuthor());
-    }
-
-    public function testGetIssueApiUrl()
-    {
-        self::assertSame($this->issueApiUrl, $this->sut->getIssueApiUrl());
     }
 
     public function testGetCreatedAt()
@@ -137,11 +109,8 @@ class GitHubIssueCommentTest extends TestCase
 
                 'siteAdmin' => false,
             ],
-            'htmlUrl'     => 'htmlUrl',
-            'apiUrl'      => 'apiUrl',
-            'issueApiUrl' => 'apiUrl',
-            'createdAt'   => '2016-08-02T17:35:14+00:00',
-            'updatedAt'   => '2016-08-02T17:35:14+00:00',
+            'createdAt' => '2016-08-02T17:35:14+00:00',
+            'updatedAt' => '2016-08-02T17:35:14+00:00',
         ];
 
         self::assertSame($expected, $this->sut->serialize());
