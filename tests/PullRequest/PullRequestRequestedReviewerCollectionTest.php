4b58c30a3b418b41b838393e47d5f2e8535d45b4
Miro Svrtan <miro@mirosvrtan.me>
2018-05-06 17:06:50 +0200

    Feat #61: Add PullRequestRequestedReviewerCollection

---
+++ tests/PullRequest/PullRequestRequestedReviewerCollectionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DevboardLib\GitHub\PullRequest;
+
+use DevboardLib\GitHub\Account\AccountType;
+use DevboardLib\GitHub\PullRequest\PullRequestRequestedReviewer;
+use DevboardLib\GitHub\PullRequest\PullRequestRequestedReviewerCollection;
+use DevboardLib\GitHub\User\UserAvatarUrl;
+use DevboardLib\GitHub\User\UserId;
+use DevboardLib\GitHub\User\UserLogin;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DevboardLib\GitHub\PullRequest\PullRequestRequestedReviewerCollection
+ * @group  unit
+ */
+class PullRequestRequestedReviewerCollectionTest extends TestCase
+{
+    /** @var array */
+    private $elements = [];
+
+    /** @var PullRequestRequestedReviewerCollection */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->elements = [
+            new PullRequestRequestedReviewer(
+                new UserId(1), new UserLogin('value'), AccountType::USER(), new UserAvatarUrl('avatarUrl'), true
+            ),
+        ];
+        $this->sut = new PullRequestRequestedReviewerCollection($this->elements);
+    }
+
+    public function testGetElements()
+    {
+        self::assertSame($this->elements, $this->sut->toArray());
+    }
+
+    public function testSerializeAndDeserialize()
+    {
+        $serialized     = $this->sut->serialize();
+        $serializedJson = json_encode($serialized);
+        self::assertEquals($this->sut, $this->sut->deserialize(json_decode($serializedJson, true)));
+    }
+}
