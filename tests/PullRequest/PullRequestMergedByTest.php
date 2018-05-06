a7dd4ce87a8be24f14c84621f0a64772c93d45e1
Miro Svrtan <miro@mirosvrtan.me>
2018-05-06 17:06:18 +0200

    Feat #59: Add PullRequestMergedBy

---
+++ tests/PullRequest/PullRequestMergedByTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DevboardLib\GitHub\PullRequest;
+
+use DevboardLib\GitHub\Account\AccountType;
+use DevboardLib\GitHub\PullRequest\PullRequestMergedBy;
+use DevboardLib\GitHub\User\UserAvatarUrl;
+use DevboardLib\GitHub\User\UserId;
+use DevboardLib\GitHub\User\UserLogin;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DevboardLib\GitHub\PullRequest\PullRequestMergedBy
+ * @group  unit
+ */
+class PullRequestMergedByTest extends TestCase
+{
+    /** @var UserId */
+    private $userId;
+
+    /** @var UserLogin */
+    private $login;
+
+    /** @var AccountType */
+    private $type;
+
+    /** @var UserAvatarUrl */
+    private $avatarUrl;
+
+    /** @var bool */
+    private $siteAdmin;
+
+    /** @var PullRequestMergedBy */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->userId    = new UserId(6752317);
+        $this->login     = new UserLogin('baxterthehacker');
+        $this->type      = new AccountType('User');
+        $this->avatarUrl = new UserAvatarUrl('https://avatars.githubusercontent.com/u/6752317?v=3');
+        $this->siteAdmin = false;
+        $this->sut       = new PullRequestMergedBy(
+            $this->userId, $this->login, $this->type, $this->avatarUrl, $this->siteAdmin
+        );
+    }
+
+    public function testGetUserId()
+    {
+        self::assertSame($this->userId, $this->sut->getUserId());
+    }
+
+    public function testGetLogin()
+    {
+        self::assertSame($this->login, $this->sut->getLogin());
+    }
+
+    public function testGetType()
+    {
+        self::assertSame($this->type, $this->sut->getType());
+    }
+
+    public function testGetAvatarUrl()
+    {
+        self::assertSame($this->avatarUrl, $this->sut->getAvatarUrl());
+    }
+
+    public function testIsSiteAdmin()
+    {
+        self::assertSame($this->siteAdmin, $this->sut->isSiteAdmin());
+    }
+
+    public function testSerialize()
+    {
+        $expected = [
+            'userId'    => 6752317,
+            'login'     => 'baxterthehacker',
+            'type'      => 'User',
+            'avatarUrl' => 'https://avatars.githubusercontent.com/u/6752317?v=3',
+            'siteAdmin' => false,
+        ];
+
+        self::assertSame($expected, $this->sut->serialize());
+    }
+
+    public function testDeserialize()
+    {
+        $serialized = json_encode($this->sut->serialize());
+        self::assertEquals($this->sut, PullRequestMergedBy::deserialize(json_decode($serialized, true)));
+    }
+}
